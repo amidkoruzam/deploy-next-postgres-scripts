e13f1708ef98d76116c80fa188d9f151d1ce6790
Dmitrii Mazurok <tmazurok@gmail.com>
2023-06-20 20:53:54 +0300

    fixes to setup script

--- server-setup.sh
+++ server-setup.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
-source .env
-ssh user@server
+source .env.prod
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-source ~/.bashrc
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 nvm install 18
 nvm alias default 18
 nvm use
 
-sudo apt update
-sudo apt upgrade -y
-sudo apt install -y postgresql postgresql-contrib nginx ufw
+npm i -g pm2
 
-sudo ufw enable
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install -y postgresql postgresql-contrib nginx ufw
+
+sudo ufw --force enable
 sudo ufw default deny incoming
 sudo ufw allow OpenSSH
 sudo ufw allow 'Nginx Full'
 
 sudo -u postgres psql -c "CREATE DATABASE $DB_NAME;"
-sudo -u postgres psql -c "CREATE USER $USERNAME WITH ENCRYPTED PASSWORD '$PASSWORD';"
-sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $USERNAME;"
+sudo -u postgres psql -c "CREATE USER $DB_USERNAME WITH ENCRYPTED PASSWORD '$DB_PASSWORD';"
+sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USERNAME;"
 
 sudo systemctl enable nginx
 sudo systemctl start nginx
